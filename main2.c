657c46a9b9eb0406a9d4a2c759ddf0b3bda45695
mungai14 <xaviermungai@gmail.com>
2026-09-23 16:34:36 +0900

    Create main2.c

---
+++ main2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int x,y;
+  scanf("%d %d",&x,&y);
+  printf("sum %d\n", x+y);
+  printf("difference %d\n", x-y);
+  printf("product %d\n", x*y);
+  printf("quotient%d\n", x\y);
+  return 0;
+}
